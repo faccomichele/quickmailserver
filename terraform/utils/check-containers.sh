0193f5f53ef3653c075d76dc568a083b84a23283
Michele Facco <faccomichele@gmail.com>
2024-09-29 11:15:51 +0700

    extra comments

--- terraform/utils/check-containers.sh
+++ terraform/utils/check-containers.sh
@@ -1,3 +1,3 @@
-# Tail and follow the deployment logs via SSH, this relies on Terraform output so it must be executed within the terraform folder
+# Print out docker ps -a command output via SSH, this relies on Terraform output so it must be executed within the terraform folder
 
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa_quick_mail_server root@$(terraform output -json | jq -r .mailserver.value.ipv4) docker ps -a
