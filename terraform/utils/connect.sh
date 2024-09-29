5f2dde64daddc99ae043def4909cf15667cea8d0
Michele Facco <faccomichele@gmail.com>
2024-09-29 10:44:23 +0700

    forgot to add the utils folder

---
+++ terraform/utils/connect.sh
@@ -0,0 +1,3 @@
+# Connect to the remote host via SSH, this relies on Terraform output so it must be executed within the terraform folder
+
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa_quick_mail_server root@$(terraform output -json | jq -r .mailserver.value.ipv4)
